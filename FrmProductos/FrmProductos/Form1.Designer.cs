5e4d8cd250730351d441fd541dc5cfca6cf8445f
Sonia Villeda <sonia08_m@hotmail.com>
2018-10-18 12:03:09 -0600

    Desarrollo del ejercicio de examen

--- FrmProductos/FrmProductos/Form1.Designer.cs
+++ FrmProductos/FrmProductos/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FrmProductos
 {
-    partial class Form1
+    partial class frmProductos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbProductos = new System.Windows.Forms.GroupBox();
+            this.lstProductos = new System.Windows.Forms.ListBox();
+            this.lblNombreCompleto = new System.Windows.Forms.Label();
+            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtValoracion = new System.Windows.Forms.TextBox();
+            this.lblValoracion = new System.Windows.Forms.Label();
+            this.txtComentarios = new System.Windows.Forms.TextBox();
+            this.lblComentarios = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.gbProductos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbProductos
+            // 
+            this.gbProductos.Controls.Add(this.btnSalir);
+            this.gbProductos.Controls.Add(this.btnAceptar);
+            this.gbProductos.Controls.Add(this.txtComentarios);
+            this.gbProductos.Controls.Add(this.lblComentarios);
+            this.gbProductos.Controls.Add(this.txtValoracion);
+            this.gbProductos.Controls.Add(this.lblValoracion);
+            this.gbProductos.Controls.Add(this.txtCorreo);
+            this.gbProductos.Controls.Add(this.lblCorreo);
+            this.gbProductos.Controls.Add(this.txtNombreCompleto);
+            this.gbProductos.Controls.Add(this.lblNombreCompleto);
+            this.gbProductos.Controls.Add(this.lstProductos);
+            this.gbProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbProductos.Location = new System.Drawing.Point(12, 12);
+            this.gbProductos.Name = "gbProductos";
+            this.gbProductos.Size = new System.Drawing.Size(677, 386);
+            this.gbProductos.TabIndex = 0;
+            this.gbProductos.TabStop = false;
+            this.gbProductos.Text = "Seleccione el producto a realizar su reseña/análisis";
+            // 
+            // lstProductos
+            // 
+            this.lstProductos.FormattingEnabled = true;
+            this.lstProductos.ItemHeight = 20;
+            this.lstProductos.Location = new System.Drawing.Point(16, 38);
+            this.lstProductos.Name = "lstProductos";
+            this.lstProductos.Size = new System.Drawing.Size(360, 284);
+            this.lstProductos.TabIndex = 0;
+            // 
+            // lblNombreCompleto
+            // 
+            this.lblNombreCompleto.AutoSize = true;
+            this.lblNombreCompleto.Location = new System.Drawing.Point(382, 38);
+            this.lblNombreCompleto.Name = "lblNombreCompleto";
+            this.lblNombreCompleto.Size = new System.Drawing.Size(137, 20);
+            this.lblNombreCompleto.TabIndex = 1;
+            this.lblNombreCompleto.Text = "Nombre Completo";
+            // 
+            // txtNombreCompleto
+            // 
+            this.txtNombreCompleto.Location = new System.Drawing.Point(386, 61);
+            this.txtNombreCompleto.Name = "txtNombreCompleto";
+            this.txtNombreCompleto.Size = new System.Drawing.Size(267, 26);
+            this.txtNombreCompleto.TabIndex = 2;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(386, 117);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(267, 26);
+            this.txtCorreo.TabIndex = 4;
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(382, 94);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(140, 20);
+            this.lblCorreo.TabIndex = 3;
+            this.lblCorreo.Text = "Correo Electrónico";
+            // 
+            // txtValoracion
+            // 
+            this.txtValoracion.Location = new System.Drawing.Point(386, 171);
+            this.txtValoracion.Name = "txtValoracion";
+            this.txtValoracion.Size = new System.Drawing.Size(267, 26);
+            this.txtValoracion.TabIndex = 6;
+            this.txtValoracion.TextChanged += new System.EventHandler(this.txtValoracion_TextChanged);
+            // 
+            // lblValoracion
+            // 
+            this.lblValoracion.AutoSize = true;
+            this.lblValoracion.Location = new System.Drawing.Point(382, 148);
+            this.lblValoracion.Name = "lblValoracion";
+            this.lblValoracion.Size = new System.Drawing.Size(84, 20);
+            this.lblValoracion.TabIndex = 5;
+            this.lblValoracion.Text = "Valoración";
+            // 
+            // txtComentarios
+            // 
+            this.txtComentarios.Location = new System.Drawing.Point(386, 232);
+            this.txtComentarios.Multiline = true;
+            this.txtComentarios.Name = "txtComentarios";
+            this.txtComentarios.Size = new System.Drawing.Size(267, 84);
+            this.txtComentarios.TabIndex = 8;
+            // 
+            // lblComentarios
+            // 
+            this.lblComentarios.AutoSize = true;
+            this.lblComentarios.Location = new System.Drawing.Point(382, 209);
+            this.lblComentarios.Name = "lblComentarios";
+            this.lblComentarios.Size = new System.Drawing.Size(99, 20);
+            this.lblComentarios.TabIndex = 7;
+            this.lblComentarios.Text = "Comentarios";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(156, 339);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(92, 34);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(427, 339);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(92, 34);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(701, 412);
+            this.Controls.Add(this.gbProductos);
+            this.Name = "frmProductos";
+            this.Text = "Reseña/Análisis de productos - AdventureWorks";
+            this.Load += new System.EventHandler(this.frmProductos_Load);
+            this.gbProductos.ResumeLayout(false);
+            this.gbProductos.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbProductos;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox txtNombreCompleto;
+        private System.Windows.Forms.Label lblNombreCompleto;
+        private System.Windows.Forms.ListBox lstProductos;
+        private System.Windows.Forms.TextBox txtComentarios;
+        private System.Windows.Forms.Label lblComentarios;
+        private System.Windows.Forms.TextBox txtValoracion;
+        private System.Windows.Forms.Label lblValoracion;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAceptar;
     }
 }
 
--- FrmProductos/FrmProductos/Form1.cs
+++ FrmProductos/FrmProductos/Form1.cs
@@ -8,13 +8,158 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace FrmProductos
 {
-    public partial class Form1 : Form
+    public partial class frmProductos : Form
     {
-        public Form1()
+
+        // Creación de la conexión
+        SqlConnection conn = new SqlConnection(@"server = (local); 
+                        integrated security = true; database = AdventureWorks2014");
+        public frmProductos()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Cargará automáticamente los productos en la lista
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmProductos_Load(object sender, EventArgs e)
+        {
+
+            // Query para traer los productos
+            string sqlSelect = @"SELECT ProductID, Name
+                                FROM Production.Product
+                                WHERE FinishedGoodsFlag = 1";
+
+            // Creamos el comando 
+            SqlCommand cmd = new SqlCommand(sqlSelect, conn);
+
+            try
+            {
+                // Abrimos Conexión
+                conn.Open();
+
+                // Ejecutamos el query
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                // Mientras lea los campos, se va a ir agregando al listbox
+                while (rdr.Read())
+                {
+                    //lstProductos.Items.Add(rdr[0]);
+                    lstProductos.Items.Add(rdr[1]);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                // Capturamos los posibles errores
+                MessageBox.Show(ex.Message + ex.StackTrace, "! Detalles en la excepción!");
+            }
+            finally
+            {
+                // Cerramos la conexión
+                conn.Close();
+
+            }
+
+        }
+
+        /// <summary>
+        /// Se encarga de Cerrar la aplicación
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            // Cerramos el formulario
+            this.Close();
+        }
+
+        /// <summary>
+        /// Se encarga de Agregar la reseña/análisis del producto elegido
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            // Crear el comando que va a llamar al Stored Procedure
+            SqlCommand cmd = new SqlCommand("sp_IngresarResena", conn);
+
+            // Establecer el comando como un Stored Procedure
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                // Verifica si se llenaron los campo
+                if (txtNombreCompleto.Text == "" || txtCorreo.Text == "" || txtValoracion.Text == "")
+
+                {
+                    MessageBox.Show("Favor llenar los campos antes de ejecutar la acción", "Información");
+                }
+                else
+                {
+                    // Verificar si esta selccionado un producto
+                    if (lstProductos.SelectedIndex == -1)
+                    {
+                        MessageBox.Show("Favor seleccionar un producto antes de ejecutar la acción de eliminar", "Información");
+                    }
+                    else
+                    {
+                        // Parámetro del Stored Procedure
+                        cmd.Parameters.Add(new SqlParameter("@productName", SqlDbType.NVarChar, 50));
+                        cmd.Parameters["@productName"].Value = lstProductos.SelectedItems[0];
+
+                        cmd.Parameters.Add(new SqlParameter("@reviewerName", SqlDbType.NVarChar, 50));
+                        cmd.Parameters["@reviewerName"].Value = txtNombreCompleto.Text;
+
+                        cmd.Parameters.Add(new SqlParameter("@reviewDate", SqlDbType.DateTime));
+                        cmd.Parameters["@reviewDate"].Value = System.DateTime.Now;
+
+                        cmd.Parameters.Add(new SqlParameter("@emailAddress", SqlDbType.NVarChar, 50));
+                        cmd.Parameters["@emailAddress"].Value = txtCorreo.Text;
+
+                        cmd.Parameters.Add(new SqlParameter("@rating", SqlDbType.Int));
+                        cmd.Parameters["@rating"].Value = txtValoracion.Text.ToString();
+
+                        cmd.Parameters.Add(new SqlParameter("@comments", SqlDbType.NVarChar, 3850));
+                        cmd.Parameters["@comments"].Value = txtComentarios.Text;
+
+                        cmd.Parameters.Add(new SqlParameter("@modifiedDate", SqlDbType.DateTime));
+                        cmd.Parameters["@modifiedDate"].Value = System.DateTime.Now;
+
+                        // Establecer la conexión
+                        conn.Open();
+
+                        // Ejecutamos el query para registar la reseña
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Reseña Registrada Exitosamente", "Información");
+                    }                 
+                                      
+                    
+                }
+                                
+            }
+            // Atrapamos las excepciones
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, "Detalles en la excepción");
+            }
+            finally
+            {
+                // Cerrar la conexión
+                conn.Close();
+            }
+
+        }
+
+        private void txtValoracion_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
